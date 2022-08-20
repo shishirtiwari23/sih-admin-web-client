4bd2dede90179900f0a155f5f6bf90a1f31f016c
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-08-20 09:11:50 +0530

    article updation ui added

--- src/pages/Articles/Manage/Manage.jsx
+++ src/pages/Articles/Manage/Manage.jsx
@@ -9,7 +9,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { ArticleContext } from "../../../utils";
+import { ArticleContext, convertToSlug, onValuesChange } from "../../../utils";
 import { useSnackbar } from "notistack";
 import { useNavigate } from "react-router-dom";
 import { CreatableMultipleSelect } from "../../../components";
@@ -253,6 +253,89 @@ const ConfirmationModal = ({ id, open, setOpen }) => {
 };
 
 const EditModal = ({ id, open, setOpen }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const { ARTICLE_API } = useContext(ArticleContext);
+  const [values, setValues] = useState({});
+  const { enqueueSnackbar } = useSnackbar();
+  const [categoryOptions, setCategoryOptions] = useState([
+    {
+      name: "Vlog",
+      value: "vlog",
+    },
+    {
+      name: "Makeup",
+      value: "makeup",
+    },
+    {
+      name: "Genz",
+      value: "genz",
+    },
+    {
+      name: "Skincare",
+      value: "skincare",
+    },
+    {
+      name: "Fitness",
+      value: "fitness",
+    },
+    {
+      name: "Couple",
+      value: "couple",
+    },
+    {
+      name: "Dance",
+      value: "dance",
+    },
+    {
+      name: "Comedy",
+      value: "comedy",
+    },
+    {
+      name: "Music",
+      value: "music",
+    },
+  ]);
+  const [category, setCategory] = useState([]);
+  async function handleAddCategory(newCategory) {
+    console.log(newCategory);
+  }
+  async function submitHandler(e) {
+    e.preventDefault();
+    const reqBody = {
+      ...values,
+
+      createdBy: {
+        id: "123",
+        userType: "Admin",
+      },
+      slug: convertToSlug(values?.title),
+    };
+    const res = await ARTICLE_API.post("/update", reqBody);
+    console.log(res);
+    if (res?.data?.status === "success")
+      enqueueSnackbar("Article Updated", { variant: "success" });
+    else enqueueSnackbar("Failed To Update Article", { variant: "error" });
+  }
+
+  useEffect(() => {
+    async function getArticle() {
+      const res = await ARTICLE_API.get("/single", {
+        params: {
+          id,
+        },
+      });
+      if (res.status === 200) {
+        const newArticle = res.data;
+        console.log(newArticle);
+        setValues(newArticle);
+      } else {
+        enqueueSnackbar("Failed To Fetch Article", { variant: "error" });
+      }
+      console.log(res);
+      setIsLoading(false);
+    }
+    getArticle();
+  }, [ARTICLE_API, enqueueSnackbar, id]);
   return (
     <div className={styles.modal}>
       <Modal
@@ -263,58 +346,70 @@ const EditModal = ({ id, open, setOpen }) => {
       >
         <Box
           sx={{
+            height: "90vh",
             position: "absolute",
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 400,
+            width: "80vw",
             bgcolor: "background.paper",
             boxShadow: 24,
+            overflow: "auto",
             p: 4,
           }}
         >
-          {/* <form onSubmit={submitHandler} action="">
-            <TextField
-              required
-              id="title"
-              value={values?.title}
-              onChange={(e) => onValuesChange(e, setValues)}
-              label="Title"
-              variant="outlined"
-            />
-            <CreatableMultipleSelect
-              required
-              options={categoryOptions}
-              width="100%"
-              setValue={setCategory}
-              id="brandSector"
-              value={category}
-              label={"Category"}
-              onAddModalSubmit={handleAddCategory}
-            />
-            <TextField
-              required
-              id="thumbnail"
-              value={values?.thumbnail}
-              onChange={(e) => onValuesChange(e, setValues)}
-              label="Thhumnail"
-              variant="outlined"
-            />
-            <TextField
-              required
-              multiline
-              maxRows={16}
-              minRows={4}
-              id="content"
-              value={values?.content}
-              onChange={(e) => onValuesChange(e, setValues)}
-              label="Content"
-              variant="outlined"
-            />
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </form> */}
+          <Typography sx={{ marginBottom: "1rem" }} variant="h5" component="h4">
+            Edit Article
+          </Typography>
+          {isLoading ? (
+            <CircularProgress />
+          ) : (
+            <form onSubmit={submitHandler} action="">
+              <TextField
+                required
+                margin="normal"
+                id="title"
+                value={values?.title}
+                onChange={(e) => onValuesChange(e, setValues)}
+                label="Title"
+                variant="outlined"
+              />
+              <CreatableMultipleSelect
+                required
+                options={categoryOptions}
+                width="100%"
+                setValue={setCategory}
+                id="brandSector"
+                value={category}
+                label={"Category"}
+                onAddModalSubmit={handleAddCategory}
+              />
+              <TextField
+                required
+                id="thumbnail"
+                value={values?.thumbnail}
+                onChange={(e) => onValuesChange(e, setValues)}
+                label="Thhumnail"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                multiline
+                maxRows={16}
+                minRows={4}
+                margin="normal"
+                id="content"
+                value={values?.content}
+                onChange={(e) => onValuesChange(e, setValues)}
+                label="Content"
+                variant="outlined"
+              />
+              <Button variant="contained" type="submit">
+                Update
+              </Button>
+            </form>
+          )}
         </Box>
       </Modal>
     </div>
